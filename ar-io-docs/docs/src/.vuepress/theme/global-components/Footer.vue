0ae166150e18bd418596d40a2bc08bc3647ebd8d
Stephen <62410139+Bobinstein@users.noreply.github.com>
2023-03-26 17:26:00 -0400

    pasted footer and css from ar.io

--- ar-io-docs/docs/src/.vuepress/theme/global-components/Footer.vue
+++ ar-io-docs/docs/src/.vuepress/theme/global-components/Footer.vue
@@ -1,10 +1,61 @@
 <template>
-  <footer class="footer">
-    <div class="footer-text">
-      <p>footer goes here</p>
-    </div>
-    <p>poopy</p>
-  </footer>
+  <footer id="tt-footer" style="">
+	<div class="tt-footer-inner">
+
+		<div class="footer-col tt-align-center-left">
+			<div class="footer-col-inner">
+
+
+				<div class="magnetic-wrap"><div class="magnetic-item subscribe-hide" style="">
+
+
+					<button data-tf-slider="bfozRHd0" data-tf-width="550" data-tf-hide-headers="" data-tf-position="left" data-tf-auto-close="1000" data-tf-iframe-props="title=ar.io Site Emails" data-tf-medium="snippet" style="all:unset;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis; height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;" data-tf-loaded="true">
+					Subscribe <i class="fas fa-paper-plane" style="padding-left: 10px;" aria-hidden="true"></i>
+				</button>
+			
+			</div></div>
+
+		</div>
+	</div>
+
+	<div class="footer-col tt-align-center order-m-last">
+		<div class="footer-col-inner">
+
+			<div class="magnetic-wrap"><div class="magnetic-item" style="">
+				<a href="./terms-and-conditions" target="_blank" rel="noopener" class="tt-btn tt-btn-link">Terms
+					and Conditions</a>
+				</div></div>
+
+			</div>
+		</div>
+
+
+		<div class="footer-col tt-align-center-right">
+			<div class="footer-col-inner">
+
+				<div class="footer-social">
+					<div class="footer-social-text"><span>Follow</span><i class="fas fa-share-alt" aria-hidden="true"></i></div>
+					<div class="social-buttons">
+						<ul>
+							<li><div class="magnetic-wrap"><a href="https://github.com/ardriveapp" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-github" aria-hidden="true"></i></a></div></li>
+								<li><div class="magnetic-wrap"><a href="https://discord.gg/HGG52EtTc2" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-discord" aria-hidden="true"></i></a></div></li>
+									<li><div class="magnetic-wrap"><a href="https://www.instagram.com/ardriveapp/" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-instagram" aria-hidden="true"></i></a></div></li>
+										<li><div class="magnetic-wrap"><a href="https://www.reddit.com/r/ar_io/" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-reddit" aria-hidden="true"></i></a></div></li>
+											<li><div class="magnetic-wrap"><a href="https://www.tiktok.com/@ardrive.io" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-tiktok" aria-hidden="true"></i></a></div></li>
+												<li><div class="magnetic-wrap"><a href="https://www.youtube.com/channel/UCL4JlkVcgUjyVz4IlKvbS_g" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-youtube" aria-hidden="true"></i></a></div></li>
+													<li><div class="magnetic-wrap"><a href="https://www.linkedin.com/company/68614891" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-linkedin" aria-hidden="true"></i></a></div></li>
+														<li><div class="magnetic-wrap"><a href="https://twitter.com/ar_io_team" class="magnetic-item not-hide-cursor" target="_blank" rel="noopener" style=""><i class="fab fa-twitter" aria-hidden="true"></i></a></div></li>
+
+														</ul>
+													</div>
+												</div>
+
+											</div>
+										</div>
+
+
+									</div>
+								</footer>
 </template>
 
 <script>
@@ -14,11 +65,7 @@ export default {
 </script>
 
 <style lang="stylus">
-.footer
-  display: flex
-  flex-direction: column
-  align-items: center
-  padding: 2.5rem 2.5rem 2rem
-.footer-text
-  color: blue
 </style>
+
+
+
