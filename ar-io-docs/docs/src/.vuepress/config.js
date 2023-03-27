8397012d806b723a72176d5708ea9cb2af5e62aa
Stephen <62410139+Bobinstein@users.noreply.github.com>
2023-03-26 22:39:40 -0400

    fixed error by removing nav

--- ar-io-docs/docs/src/.vuepress/config.js
+++ ar-io-docs/docs/src/.vuepress/config.js
@@ -38,59 +38,58 @@ module.exports = {
     editLinkText: "",
     lastUpdated: true,
     logo: "/images/elephant-light.png",
-    nav: [
-      {
-        text: "LEARN",
-        items: [
-          { type: "label", text: "About ar.io" },
-          { text: "What is ar.io?", link: "" },
-          { text: "Why ar.io?", link: "" },
-          { type: "label", text: "About Arweave" },
-          { text: "What is Arweave?", link: "" },
-          { text: "What is the permaweb?", link: "" },
-          { text: "What is a gateway?", link: "" },
-          { text: "The problem of long-term data storage" },
-        ],
-      },
-      {
-        text: "USE",
-        items: [
-          { text: "ArDrive", link: "" },
-          { text: "ArNS (Beta)", link: "" },
-          { text: "ar.io Gateways", link: "" },
-          { text: "Wallet", link: "" },
-          { text: "Pricing Calculator", link: "" },
-          { text: "Viewblock", link: "" },
-        ],
-      },
-      {
-        text: "DEVELOP",
-        items: [
-          { text: "CLI", link: "/ardrive-cli/" },
-          { text: "ArDrive Core-JS", link: "/ardrive-core/" },
-        ],
-      },
-      {
-        text: "ENGAGE",
-        items: [
-          { text: "Newsletter", link: "" },
-          { text: "Blog", link: "" },
-          { text: "Socials", link: "" },
-          { text: "CCC", link: "" },
-          { text: "The Arcast", link: "" },
-          { text: "Merch Store", link: "" },
-        ],
-      },
-      {
-        text: "ABOUT",
-        items: [
-          { text: "The Story", link: "" },
-          { text: "The Foundation", link: "" },
-          { text: "Press", link: "" },
-          { text: "Contact", link: "" },
-        ],
-      },
-    ],
+    // nav: [
+    //   {
+    //     text: "LEARN",
+    //     items: [
+    //       { text: "What is ar.io?", link: "" },
+    //       { text: "Why ar.io?", link: "" },
+    //       { type: "label", text: "About Arweave" },
+    //       { text: "What is Arweave?", link: "" },
+    //       { text: "What is the permaweb?", link: "" },
+    //       { text: "What is a gateway?", link: "" },
+    //       { text: "The problem of long-term data storage", link: '' },
+    //     ],
+    //   },
+    //   {
+    //     text: "USE",
+    //     items: [
+    //       { text: "ArDrive", link: "" },
+    //       { text: "ArNS (Beta)", link: "" },
+    //       { text: "ar.io Gateways", link: "" },
+    //       { text: "Wallet", link: "" },
+    //       { text: "Pricing Calculator", link: "" },
+    //       { text: "Viewblock", link: "" },
+    //     ],
+    //   },
+    //   {
+    //     text: "DEVELOP",
+    //     items: [
+    //       { text: "CLI", link: "/ardrive-cli/" },
+    //       { text: "ArDrive Core-JS", link: "/ardrive-core/" },
+    //     ],
+    //   },
+    //   {
+    //     text: "ENGAGE",
+    //     items: [
+    //       { text: "Newsletter", link: "" },
+    //       { text: "Blog", link: "" },
+    //       { text: "Socials", link: "" },
+    //       { text: "CCC", link: "" },
+    //       { text: "The Arcast", link: "" },
+    //       { text: "Merch Store", link: "" },
+    //     ],
+    //   },
+    //   {
+    //     text: "ABOUT",
+    //     items: [
+    //       { text: "The Story", link: "" },
+    //       { text: "The Foundation", link: "" },
+    //       { text: "Press", link: "" },
+    //       { text: "Contact", link: "" },
+    //     ],
+    //   },
+    // ],
   },
 
   /**
